e28ebef39d255fc0c313d178121f6386f0c3eafb
candra09 <72352882+candra09@users.noreply.github.com>
2021-12-21 03:21:11 +0700

    Update item_list.dart

--- lib/list/item_list.dart
+++ lib/list/item_list.dart
@@ -1,95 +1,142 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_project_1/models/cart.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-class ItemList extends StatelessWidget {
-//DEFINISIKAN VARIABLE UNTUK MENAMPUNG DATA LIST CART
-  final List<Cart> carts;
+class ItemList extends StatefulWidget {
+  num get harga => null;
 
-//BUAT CONSTRUCTOR UNTUK MEMINTA DATA LIST CARTNYA
-  ItemList(this.carts);
+  // ItemList({Key? key}) : super(key: key);
 
-  void setState(Null Function() param0) {}
+  @override
+  _ItemListState createState() => _ItemListState();
+}
 
+class _ItemListState extends State<ItemList> {
+  List<dynamic> items = [];
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 360,
-      width: double.infinity,
-      margin: EdgeInsets.only(top: 0),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Slidable(
-            //enable slidable in list
-            key: Key(carts[index].id), //set key
-            // you have to set key, other wise, after removing item from list, -
-            // - slidable will be opened.
-            actionPane: SlidableDrawerActionPane(),
-            actionExtentRatio: 0.15,
-            child: Card(
-              margin: EdgeInsets.only(top: 10, left: 15, right: 15),
-              //UNTUK KETEBALAN AGAR MEMBENTUK SHADOW SENILAI 8
-              elevation: 8,
-              //CHILD DARI CARD MENGGUNAKAN LISTTILE AGAR LEBIH MUDAH MENGATUR AREANYA
-              //KARENA SECARA DEFAULT LISTTILE TELAH TERBAGI MENJADI 3 BAGIAN
-              //POSISI KIRI (LEADING), TENGAH (TITLE), BAWAH TENGAH (SUBTITLE) DAN KANAN(TRAILING)
-              //SEHINGGA KITA HANYA TINGGAL MEMASUKKAN TEKS YANG SESUAI
-              child: ListTile(
-                leading: Icon(
-                  carts[index].type == 'out'
-                      ? Icons.subdirectory_arrow_left
-                      : Icons.subdirectory_arrow_right,
-                  color: carts[index].type == 'out'
-                      ? Colors.redAccent
-                      : Colors.lightGreen,
-                ),
-                title: Text(
-                  carts[index].title,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-
-                // subtitle: Text(carts[index].description),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(carts[index].description),
-                    SizedBox(height: 7),
-                    Text(carts[index].date,
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-
-                trailing: Text(
-                  "Rp " + carts[index].price.toStringAsFixed(0),
+        height: 360,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 1),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(0.1),
+              child: FlatButton(
+                splashColor: Colors.blue,
+                onPressed: () {
+                  _initData();
+                },
+                child: Text(
+                  'Refresh',
                   style: TextStyle(
-                      color: carts[index].type == 'out'
-                          ? Colors.redAccent
-                          : Colors.lightGreen),
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
                 ),
               ),
             ),
-            secondaryActions: [
-              //action button to show on tail
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.redAccent),
-                child: Icon(Icons.delete),
-                onPressed: () {
-                  //delete action for this button
-                  carts.removeWhere((element) {
-                    return element.id == carts[index].id;
-                  }); //go through the loop and match content to delete from list
-                  setState(() {
-                    //refresh UI after deleting element from list
-                  });
-                },
-              ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  var item = items[index];
+                  return Slidable(
+                    // key: Key(carts[index].id),
+                    actionPane: SlidableDrawerActionPane(),
+                    actionExtentRatio: 0.15,
+                    child: Card(
+                      margin: EdgeInsets.only(top: 10, left: 15, right: 15),
+                      elevation: 8,
+                      child: ListTile(
+                        leading: Icon(
+                          item['type'] == 'out'
+                              ? Icons.subdirectory_arrow_left
+                              : Icons.subdirectory_arrow_right,
+                          color: item['type'] == 'out'
+                              ? Colors.redAccent
+                              : Colors.lightGreen,
+                        ),
+                        title: Text(
+                          item['judul'] ?? 'judul',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
 
-              //add more action buttons here.
-            ],
-          );
-        },
-        itemCount: carts.length,
+                        // subtitle: Text(carts[index].description),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(item['deskripsi'] ?? 'deskripsi'),
+                            SizedBox(height: 7),
+                            Text(item['tanggal'] ?? 'tanggal',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+
+                        trailing: Text(
+                          "Rp ${item['harga'] ?? ' 0'} ",
+                          style: TextStyle(
+                              color: item['type'] == 'out'
+                                  ? Colors.redAccent
+                                  : Colors.lightGreen),
+                        ),
+                      ),
+                    ),
+                    secondaryActions: [
+                      //action button to show on tail
+                      ElevatedButton(
+                        style:
+                            ElevatedButton.styleFrom(primary: Colors.redAccent),
+                        child: Icon(Icons.delete),
+                        onPressed: () {
+                          _hapusFB(item['id']);
+                        },
+                      ),
+
+                      //   //add more action buttons here.
+                    ],
+                  );
+                },
+                itemCount: items.length,
+              ),
+            )
+          ],
+        ));
+  }
+
+  void _hapusFB(String id) async {
+    await FirebaseFirestore.instance
+        .collection('catat_barang')
+        .doc(id)
+        .delete();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('item telah dihapus'),
       ),
     );
+    setState(() {
+      items = items.where((element) => element['id'] != id).toList();
+    });
+  }
+
+  void _initData() async {
+    var colls =
+        await FirebaseFirestore.instance.collection('catat_barang').get();
+    setState(() {
+      items = [];
+      colls.docs.forEach((element) {
+        var data = element.data();
+
+        items.add({
+          'tanggal': data['tanggal'],
+          'judul': data['judul'],
+          'deskripsi': data['deskripsi'],
+          'harga': data['harga'],
+          'type': data['type'],
+          'id': element.id,
+        });
+      });
+    });
   }
 }
